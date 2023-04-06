64ca1625a8a9ffa5126394d6cf7e021c594a153e
Rania khalifa <rania.khalifa96@gmail.com>
2023-04-06 06:34:55 +0300

    Add the basic styles and configs

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,15 @@
-import React from 'react';
+import React, { useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 
 function App() {
+  const [cells, setCells] = useState(Array(9).fill(''));
+  const [player, setPlayer] = useState('X');
+  const [winner, setWinner] = useState('');
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className='tic-tac-toe'>
+      <h1 className='title'> TIC-TAC-TOE </h1>
     </div>
   );
 }
